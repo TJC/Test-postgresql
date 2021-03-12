be431935ca16ece71a8228f625194826f5c44e49
Toby Corkindale <tjc@wintrmute.net>
2021-03-12 21:50:56 +1100

    Checking in changes prior to tagging of version 1.28.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 78d357b..e68db05 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,10 @@ Revision history for Perl extension Test::PostgreSQL.
     
     {{$NEXT}}
     
    +1.28 2021-03-12T10:50:46Z
    +    - Increment port by random amount when trying to find a good port
    +    - Allow using beta versions
    +
     1.27 2018-10-17T02:06:08Z
         - Use pg_version attribute to better handle -b and -f psql switches
           depending on installed PostgreSQL version

--- lib/Test/PostgreSQL.pm
+++ lib/Test/PostgreSQL.pm
@@ -13,7 +13,7 @@ use File::Which;
 use POSIX qw(SIGQUIT SIGKILL WNOHANG getuid setuid);
 use User::pwent;
 
-our $VERSION = '1.27';
+our $VERSION = '1.28';
 our $errstr;
 
 # Deprecate use of %Defaults as we want to remove this package global
